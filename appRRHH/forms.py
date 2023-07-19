ad29b7cb4922d3c7ff9f914521e3385d262ca30d
Carlos Vasquez <enrique-vasquez1@outlook.es>
2023-07-19 17:29:19 -0400

    Se Guarda la interfaz de listado de solicitudes

--- appRRHH/forms.py
+++ appRRHH/forms.py
@@ -17,6 +17,10 @@ class LicenciaForm(ModelForm):
 
         }
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)               #self.instance.pk  sirve para objetos creados al inicio
+        super().__init__(*args, **kwargs)               #self.instance.pk  sirve para objetos creados al inicio}
+        for form in self.visible_fields():
+            # form.field.widget.attrs['class'] = 'form-control'
+            form.field.widget.attrs['autocomplete'] = 'off'
+            self.fields['rut'].widget.attrs['autofocus'] = True
         if not self.instance.pk:                        #Solo cuando se crea la solicitud su estado sera por defecto "En Proceso"
             self.fields['estado'].initial="En Proceso"
