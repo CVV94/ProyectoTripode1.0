6b19cb2a2c2c6f03a4723a974eed9e06c1013daa
Carlos Vasquez <enrique-vasquez1@outlook.es>
2023-07-26 10:52:47 -0400

    Se guarda el avance de edicion de datos laborales

--- appTrabajador/urls.py
+++ appTrabajador/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('solicitud-licencia/',views.solicitud_licencia, name='solicitud_licencia'),
     path('modulo/trabajador/',views.modulo_Trabajador, name='modulo_trabajador'),
     path('listado/licencias/',views.misSolicitudes, name='listaLicencias'),
+    path('consulta/', views.consulta_personalizada, name='consulta_personalizada'),
+    path('editar-informacion/<str:rut_trabajador>/',views.editar_informacion_personal, name='editar_informacion_personal')
 ]
--- appTrabajador/views.py
+++ appTrabajador/views.py
@@ -55,3 +55,63 @@ def misSolicitudes(request):
 
         # Pasar las solicitudes al contexto del template para visualizarlas
     return render(request, 'trabajador/solicitud_licencia_success.html', {'solicitudes': solicitudes})
+
+def consulta_personalizada(request):
+    # Obtenemos el 'nombre_perfil' del usuario desde la sesión
+    nombre_perfil = request.session.get('nombre_perfil', None)
+
+    # Inicializamos 'result' como un diccionario vacío
+    result = {}
+
+    # Verificamos si el usuario ha iniciado sesión y si su 'nombre_perfil' es 'Trabajador'
+    if nombre_perfil == 'Trabajador':
+        # Obtenemos el 'rut' del trabajador desde la sesión
+        rut_trabajador = request.session.get('rut_trabajador', None)
+        if rut_trabajador:
+            # Realizamos la consulta personalizada utilizando el 'rut_trabajador'
+            trabajador = Trabajador.objects.filter(rut=rut_trabajador).first()
+            if trabajador:
+                result['trabajador'] = trabajador
+                result['cargas_familiares'] = trabajador.cargafamiliar_set.all()
+                result['contactos_emergencia'] = trabajador.contactoemergencia_set.all()
+
+    # Pasamos los resultados a la plantilla para mostrarlos
+    return render(request, 'trabajador/consulta_personalizada.html', {'result': result})
+
+def editar_informacion_personal(request, rut_trabajador):
+    try:
+        trabajador = Trabajador.objects.get(rut=rut_trabajador)
+    except Trabajador.DoesNotExist:
+        # Manejo del caso donde el trabajador no existe
+        return redirect('ruta_de_vista_de_error')  # Cambia 'ruta_de_vista_de_error' por la vista de manejo de error que desees
+
+    if request.method == 'POST':
+        # Si se envió el formulario con los datos editados
+        trabajador.nombre = request.POST.get('nombre', trabajador.nombre)
+        trabajador.sexo = request.POST.get('sexo', trabajador.sexo)
+        trabajador.direccion = request.POST.get('direccion', trabajador.direccion)
+        trabajador.telefono = request.POST.get('telefono', trabajador.telefono)
+
+        # Cargas Familiares
+        num_cargas = int(request.POST.get('num_cargas', 0))
+        trabajador.cargafamiliar_set.all().delete()  # Eliminamos las cargas familiares anteriores
+        for i in range(num_cargas):
+            cf_nombre = request.POST.get(f'cf_nombre_{i}', '')
+            cf_parentesco = request.POST.get(f'cf_parentesco_{i}', '')
+            cf_sexo = request.POST.get(f'cf_sexo_{i}', '')
+            trabajador.cargafamiliar_set.create(cf_nombre=cf_nombre, parentesco=cf_parentesco, cf_sexo=cf_sexo)
+
+        # Contactos de Emergencia
+        num_contactos = int(request.POST.get('num_contactos', 0))
+        trabajador.contactoemergencia_set.all().delete()  # Eliminamos los contactos de emergencia anteriores
+        for i in range(num_contactos):
+            ce_nombre = request.POST.get(f'ce_nombre_{i}', '')
+            ce_relacion = request.POST.get(f'ce_relacion_{i}', '')
+            ce_telefono = request.POST.get(f'ce_telefono_{i}', '')
+            trabajador.contactoemergencia_set.create(ce_nombre=ce_nombre, relacion_con_trabajador=ce_relacion,
+                                                     ce_telefono=ce_telefono)
+
+        trabajador.save()
+        return redirect('consulta_personalizada')  # Cambia 'ruta_de_vista_de_consulta_personalizada' por la vista de consulta personalizada que creaste
+
+    return render(request, 'trabajador/editar_informacion_personal.html', {'trabajador': trabajador})
